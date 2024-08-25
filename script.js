d8dd0d4826ddef1e71ef9ebdd516d1fec2869c11
nekosoffy <161766793+nekosoffy@users.noreply.github.com>
2024-08-25 14:40:40 -0300

    Add operator-picking and number display functions

--- script.js
+++ script.js
@@ -15,20 +15,62 @@ function divide(a,b) {
 }
 
 let firstNumber;
-let operator;
+let operator = "";
+let operatorPicked = false;
 let secondNumber;
+let firstNumberPicked = false;
+let secondNumberPicked = false;
+let cleaned = false;
+const display = document.querySelector(".display");
 
 function operate() {
+    let result;
     if (operator = "+") {
-        add(firstNumber,secondNumber);
+        return result = add(firstNumber,secondNumber);
     }
     if (operator = "-") {
-        subtract(firstNumber,secondNumber);
+        return result = subtract(firstNumber,secondNumber);
     }
-    if (operator = "*") {
-        multiply(firstNumber,secondNumber);
+    if (operator = "∗") {
+        return result = multiply(firstNumber,secondNumber);
     }
-    if (operator = "/") {
-        divide(firstNumber,secondNumber);
+    if (operator = "÷") {
+        return result = divide(firstNumber,secondNumber);
     }
+}
+
+function numberDisplay() {
+    const digitButtons = document.querySelectorAll(".digits button");
+    digitButtons.forEach(button => {
+        button.addEventListener("click", () => {
+        if (display.textContent == "0") {
+            display.textContent = "";
+        } else if (operator != "" && cleaned == false) {
+            display.textContent = "";
+            cleaned = true;
+        }
+        display.textContent += button.textContent;
+        getNumbers();
+    });
+});
+}
+
+function pickOperator() {
+    const operatorButtons = document.querySelectorAll(".symbols button");
+    operatorButtons.forEach(button => {
+        button.addEventListener("click", () => {
+            if ((button.textContent != ",") || (button.textContent != "")) {
+                firstNumberPicked = true;
+                operator = button.textContent;
+                operatorPicked = true;
+            }
+            if ((secondNumberPicked == true) && (operatorPicked == true)) {
+                firstNumber = operate();
+                display.textContent = firstNumber;
+                secondNumberPicked = false;
+                operator = button.textContent;
+            }
+        console.log(operator);
+    });
+});
 }
